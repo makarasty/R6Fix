c1710b9c761f16b74697982ef960f8ac52706fe6
MaKarastY <71918286+makarasty@users.noreply.github.com>
2023-01-27 23:01:12 +0200

    Add files via upload

--- R6Fix.bat
+++ R6Fix.bat
@@ -1,14 +1,17 @@
-@REM only for four-core processors
 @echo off
-PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=8" > nul
+PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=0x0003" > nul
 if %ERRORLEVEL% neq 0 goto Error
 timeout /t 1 /nobreak > nul
-PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=15" > nul
+PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=0x000F" > nul
 if %ERRORLEVEL% neq 0 goto Error
 echo All done, the script will close in 10 seconds, good luck!
 timeout /t 10 /nobreak > nul
 exit
+
 :Error
-echo First you need to run Rainbow Six Siege
+echo First you need to run Rainbow Six Siege!
+echo.
+echo If Rainbow Six Siege is already running, run this as an administrator!
 timeout /t -1 /nobreak > nul
+
 @REM all this coded by MaKarastY
