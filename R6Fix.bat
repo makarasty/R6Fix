b44fa45aaaa59144804e0a33e9e34d8c23d5084a
makarasty <makarasty123@gmail.com>
2024-07-16 13:26:32 +0300

    fix, using PowerShell, support all cores

--- R6Fix.bat
+++ R6Fix.bat
@@ -1,16 +1,15 @@
 @echo off
-PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=0x0003" > nul
-if %ERRORLEVEL% neq 0 goto Error
-PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=0x000F" > nul
-if %ERRORLEVEL% neq 0 goto Error
-echo All done, the script will close in any seconds, good luck!
-timeout /t 3 /nobreak > nul
-exit
 
-:Error
-echo First you need to run Rainbow Six Siege!
-echo.
-echo If Rainbow Six Siege is already running, run this as an administrator!
-timeout /t -1 /nobreak > nul
+PowerShell -Command "$Process = Get-Process RainbowSix -ErrorAction SilentlyContinue; if ($Process) {$Process.ProcessorAffinity = 0x0001; Start-Sleep -Milliseconds 500; $allCoresAffinity = [int][math]::Pow(2, [Environment]::ProcessorCount) - 1; $Process.ProcessorAffinity = $allCoresAffinity; exit 0} else {exit 1}" > nul
+
+if %ERRORLEVEL% equ 0 (
+	echo All done, the script will close in 3 seconds, good luck!
+	timeout /t 3 /nobreak > nul
+) else (
+	echo First you need to run Rainbow Six Siege!
+	echo.
+	echo If Rainbow Six Siege is already running, run this as an administrator!
+	pause > nul
+)
 
 @REM all this coded by MaKarastY
